f235f9343f6586c29190d6d432f8f1748a14e265
malay190 <malaykumar089@gmail.com>
2024-08-25 22:03:35 +0530

    updated the logic of the game

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,1 +1,48 @@
 'use strict';
+
+let number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+console.log(number);
+document.querySelector('.btn.again').addEventListener('click', function () {
+  score = 20;
+  number = Math.trunc(Math.random() * 20) + 1;
+  console.log(number);
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ Enter the number...';
+  }
+  //when the guess is correct
+  else if (guess === number) {
+    if (highScore < score) {
+      highScore = score;
+    }
+    document.querySelector('.message').textContent = '🥇Correct Guess..';
+    document.querySelector('.number').textContent = number;
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.highscore').textContent = highScore;
+  }
+  //when number is too high
+  else if (guess !== number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > number ? '📈Too High..' : '📉 Too Low..';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = '😥 You Lost...';
+    }
+  }
+});
